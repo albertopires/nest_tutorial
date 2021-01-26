fed07b684b98d488c31e79ef2ae338b2852298c6
Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
2021-01-26 18:20:39 -0300

    exceptions

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,16 +1,28 @@
 import { CustomerService } from './customer.service';
 
-import { Body, Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, ParseIntPipe, Post, Req } from '@nestjs/common';
 import { CustomerDto } from './customer.dto';
 
 @Controller('customer')
 export class CustomerController {
     constructor(private readonly customerService: CustomerService) {}
 
-    @Get(':id')
+    @Get('/id/:id')
     getHello(@Param('id') id: string): string {
         console.log(`This action returns a #${id} customer`);
         return this.customerService.getHelloCustomer();
+    }
+
+    @Get('/error/:op')
+    generateError(@Param('op', ParseIntPipe) op: number) : string {
+        if (op == 0) {
+            console.log("no error");
+        }
+        if (op == 1) {
+            console.log("error");
+            throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+        }
+        return "ok";
     }
 
     @Post()
