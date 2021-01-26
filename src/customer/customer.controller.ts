f02d8e1a869df1d39af78cbd8504b72107e2524e
Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
2021-01-26 19:01:55 -0300

    file upload and save on filesystem

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,20 +1,22 @@
 import { CustomerService } from './customer.service';
 
-import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, ParseIntPipe, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, ParseIntPipe, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { CustomerDto } from './customer.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { writeFileSync } from 'fs';
 
 @Controller('customer')
 export class CustomerController {
-    constructor(private readonly customerService: CustomerService) {}
+    constructor(private readonly customerService: CustomerService) { }
 
-    @Get('/id/:id')
+    @Get('id/:id')
     getHello(@Param('id') id: string): string {
         console.log(`This action returns a #${id} customer`);
         return this.customerService.getHelloCustomer();
     }
 
-    @Get('/error/:op')
-    generateError(@Param('op', ParseIntPipe) op: number) : string {
+    @Get('error/:op')
+    generateError(@Param('op', ParseIntPipe) op: number): string {
         if (op == 0) {
             console.log("no error");
         }
@@ -25,11 +27,22 @@ export class CustomerController {
         return "ok";
     }
 
-    @Post()
+    @Post('payload')
     runPost(@Body() customerDto: CustomerDto): string {
-        console.log('customer.name: '+customerDto.name);
-        console.log('customer.Age : '+customerDto.age);
-        console.log('customer.Type: '+customerDto.type);
+        console.log('customer.name: ' + customerDto.name);
+        console.log('customer.Age : ' + customerDto.age);
+        console.log('customer.Type: ' + customerDto.type);
         return 'null';
+    }
+
+    @Post('upload')
+    @UseInterceptors(FilesInterceptor('file'))
+    uploadFile(@UploadedFiles() file) {
+        file.forEach(function (value) {
+            console.log(value);
+            console.log('===================');
+            writeFileSync("/tmp/" + value.originalname + '_tst', value.buffer);
+        })
+        //console.log(file);
     }
 }
