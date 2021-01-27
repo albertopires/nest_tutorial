c4924d87d802809b104f093e656ca23533e5b794
Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
2021-01-27 19:58:07 -0300

    basic database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerController } from './customer/customer.controller';
 import { CustomerService } from './customer/customer.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot()],
   controllers: [AppController, CustomerController],
   providers: [AppService, CustomerService],
 })
 export class AppModule {}
+/*
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5431,
+      username: 'alberto',
+      password: 'Teste123.',
+      database: 'claro',
+      entities: [],
+      synchronize: true,
+      */
